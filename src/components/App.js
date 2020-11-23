f291fcd8e9077c5b490221adce022fc2b2f16abe
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2020-11-23 14:27:33 +0530

    Update 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,48 @@
-import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-const App = () => {
-  const [currentTime,setCurrentTime] = useState(0);
-  const handleKeyDown =(event)=>{
-    if(event.key === 'Enter') {
-      let time = parseInt(event.target.value);
-      
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTime: 0, timerId: undefined };
+  }
+  startTimer = (time) => {
+    if (isNaN(time)) {
+      this.setState({ currentTime: 0 });
+    } else {
+      if (this.state.timerId) clearInterval(this.state.timerId);
+      this.setState({ currentTime: time });
+      let timerId = setInterval(() => {
+        this.setState((prevState) => {
+          if (this.state.currentTime > 0) {
+            this.setState({ currentTime: prevState.currentTime - 1 });
+          } else {
+            clearInterval(this.state.timerId);
+          }
+        });
+      }, 1000);
+      this.setState({ timerId: timerId });
     }
   };
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={handleKeyDown} /> sec.
-        </h1>
+  handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let time = parseInt(event.target.value, 10);
+      this.startTimer(time);
+    }
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <div id="whole-center">
+          <h1>
+            Reverse countdown for
+            <input id="timeCount" onKeyDown={this.handleKeyDown} /> sec.
+          </h1>
+        </div>
+        <div id="current-time">{this.state.currentTime}</div>
       </div>
-      <div id="current-time">{currentTime}</div>
-    </div>
-  )
+    );
+  }
 }
-
 
 export default App;
